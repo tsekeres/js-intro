79548096cb50464f62b267a134610f9a75510fbb
Tad Sekeres <twsekeres@gmail.com>
2021-01-16 11:08:15 -0600

    variables and math

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log("Tad Sekeres");
+
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+// console.log(firstName + ' ' + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+// shortcut for logging
+
+firstName = 'Tad';
+lastName = 'Sekeres';
+
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// const favoriteFood = 'Burritos';
+// console.log(favoriteFood);
+
+favoriteFood = 'Pizza';
+// console.log(favoriteFood);
+
+// won't work because const variable can't be changed
+
+// END Variables
+// start DATA TYPES
+
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+// ******************end DATA TYPES
+
+// ***************start MATH
+
+
+
+// ********************end MATH
