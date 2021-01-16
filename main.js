08c427080580c03248067cf7270eba8cb691213c
Tad Sekeres <twsekeres@gmail.com>
2021-01-16 13:09:32 -0600

    function logic

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log("Tad Sekeres");
+
+// function thisIsTrue(arg) {
+//   if (arg) {
+//   console.log('true');
+//   } else {
+//     console.log('false');
+//   }
+// } 
+
+// thisIsTrue(false);
+
+// function thisIsTrue(arg) {
+//   if (arg === 'nice' || arg === 'Nice') {
+//   console.log('You are ${arg}');
+//   } else {
+//     console.log('false');
+//   }
+// } 
+
+// thisIsTrue('Nice');
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1 === 'nice' && arg2 === 'Nice') {
+//   console.log(`You are ${arg1}.`);
+//   } else {
+//     console.log('false');
+//   }
+// } 
+
+// thisIsTrue('nice', 'Nice');
+
+function thisIsTrue(arg1, arg2) {
+  if (arg1 !== 'nice' && arg2 !== 'Nice') {
+  console.log(`You are ${arg1}.`);
+  } else {
+    console.log('false');
+  }
+} 
+
+thisIsTrue('nice', 'Nice');
